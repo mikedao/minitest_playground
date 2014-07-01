d577935f2ef4127b0d5ffc447f39070593f2d4e9
Josh Cheek <josh.cheek@gmail.com>
2014-06-30 22:22:47 -0600

    Add a test to capture the output

---
+++ test/capture_output_test.rb
@@ -0,0 +1,14 @@
+require_relative 'test_helper'
+
+class CaptureOutputTest < Minitest::Test
+  def hello_world
+    puts "People told me slow my roll I'm screaming out fuck that, Imma do just what I want lookin’ ahead no turnin’ back"
+  end
+
+  def test_can_capture_output
+    output = ""
+    hello_world
+    assert_includes output, "no turnin' back"
+  end
+end
+
