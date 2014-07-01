90e951bf4699e982842059f5de9fcce1347c76f3
Josh Cheek <josh.cheek@gmail.com>
2014-07-01 11:33:06 -0600

    Remove fancy apostrophes

--- test/capture_output_test.rb
+++ test/capture_output_test.rb
@@ -2,7 +2,7 @@ require_relative 'test_helper'
 
 class CaptureOutputTest < Minitest::Test
   def hello_world
-    puts "People told me slow my roll I'm screaming out fuck that, Imma do just what I want lookin’ ahead no turnin’ back"
+    puts "People told me slow my roll I'm screaming out fuck that, Imma do just what I want lookin' ahead no turnin' back"
   end
 
   def test_can_capture_output
