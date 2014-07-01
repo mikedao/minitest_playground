af3d83c97bd3afe5dc6e828b4d30bbaeddeccadc
Josh Cheek <josh.cheek@gmail.com>
2014-06-30 20:39:08 -0600

    Add a test to show that each test runs in its own instance

---
+++ test/each_test_runs_in_its_own_instance_test.rb
@@ -0,0 +1,11 @@
+require_relative 'test_helper'
+
+# Prove in whatever way satisfies you that that each
+# test runs in its own instance
+#
+# But what does that matter? Why am I bothering to tell you this?
+# Something to ponder... don't let it keep you up tonight, though ;)
+
+class EachTestRunsInItsOwnInstanceTest < Minitest::Test
+
+end
